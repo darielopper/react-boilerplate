8d4ab5430920b222feb82ce5276a3077c323b4f7
dramosv90 <vdariel90@gmail.com>
2019-04-29 18:57:31 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+    <div>
+        <h1>Hello World, from React App!!</h1>
+    </div>
+)
+
+ReactDOM.render(<App/>, document.getElementById('root'))
