aa2fe515e62ff2bbef5c5800a78f631ed14d654c
dramosv90 <vdariel90@gmail.com>
2019-05-03 13:59:31 -0400

    test App component

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,7 +1,9 @@
-import App from '../src/components/App'
-import { shallow, configure } from 'enzyme'
 import React from 'react'
+import App from '../src/components/App'
 import Adapter from 'enzyme-adapter-react-16'
+import sinon from 'sinon' //For mockery and click spies
+import { shallow, configure, mount, render } from 'enzyme'
+
 configure({ adapter: new Adapter() })
 
 describe('App component', () => {
@@ -9,5 +11,22 @@ describe('App component', () => {
         expect(shallow(
             <App />
         )).toMatchSnapshot()
+    })
+
+    test('should mount correctly', () => {
+        expect(mount(
+            <App />
+        )).toMatchSnapshot()
+    })
+
+    test('should render correctly', () => {
+        expect(render(
+            <App />
+        )).toMatchSnapshot()
+    })
+
+    test('have default text', () => {
+        const wrapper = shallow(<App />)
+        expect(wrapper.find('h1').text()).toContain('Hello World')
     })
 })
